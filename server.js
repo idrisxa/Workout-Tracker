d0553fade142f78bbbe3c515e3d9e17a377c2d5a
Idris <idrisadebisi@live.com>
2019-12-04 12:59:31 -0500

    adding databaseUrl

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongojs = require("mongojs");
 const logger = require("morgan");
 const path = require("path");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 
 const app = express();
@@ -19,6 +19,25 @@ const collections = ["exercise"];
 
 //mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${databaseUrl}`, { useNewUrlParser: true,  useUnifiedTopology: true});
 const db = mongojs(databaseUrl, collections);
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutPlanner", { useNewUrlParser: true });
+
+// const Schema = mongoose.Schema;
+
+// const workoutSchema = new Schema({
+//     title: {
+//         type: String
+//     },
+//     exercise: [{
+//         type: String
+//     }],
+//     workout: {
+//       type: String
+//     }
+
+// });
+
+// const workouts = mongoose.model("workouts", workoutSchema);
 
 db.on("error", error => {
     console.log("Database Error:", error);
@@ -116,3 +135,65 @@ db.on("error", error => {
   app.listen(3000, () => {
     console.log("App running on port 3000!");
   });
+/////////////////////////////////////////////////////////
+
+//   const express = require('express');
+// const path = require('path');
+// const mongoose = require("mongoose");
+
+// const PORT = process.env.PORT || 8080;
+
+// const app = express();
+
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutPlanner", { useNewUrlParser: true });
+
+// const Schema = mongoose.Schema;
+
+// const workoutSchema = new Schema({
+//     workoutName: {
+//         type: String
+//     },
+//     exercises: [{
+//         type: String
+//     }]
+// });
+
+// const workouts = mongoose.model("workouts", workoutSchema);
+
+// app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+// });
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./public/main.html"));
+// });
+
+// app.get("/workouts", async function(req, res) {
+//    let data = await workouts.find({})
+//    res.send(data)
+// });
+
+// app.post("/workout", async function(req, res) {
+//     let data = await workouts.find( {workoutName: req.body.name}, {exercises: 1, _id: 0} )
+//     res.send(data)
+// });
+ 
+// app.post("/addExercise", async function(req, res) {
+//     let data = await workouts.updateOne( {workoutName: req.body.workout}, {$push: {exercises: req.body.exercise} } );
+// });
+
+// app.post("/removeExercise", async function(req, res) {
+//     let data = await workouts.updateOne( {workoutName: req.body.workout}, { $pull: {exercises: req.body.exercise} } );
+// });
+
+// app.post("/createWorkout", async function(req,res) {
+//     workouts.create({workoutName: req.body.name});
+// });
+
+// app.post("/deleteWorkout", async function(req,res) {
+//     let data = await workouts.deleteOne({workoutName: req.body.workout});
+// });
